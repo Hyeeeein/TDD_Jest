c55e52e228ac9b73396d79b2bdc44bd9002132e7
Hyeeeein <khi_9866@naver.com>
2023-05-22 15:47:23 +0900

    설명 추가

--- src/calculator.js
+++ src/calculator.js
@@ -14,6 +14,7 @@ class Calculator {
   add(num) {
     const sum = this.value + num;
     if (sum > 100) {
+      // 따로 에러 테스트 코드를 작성해주지 않으면 94.28% 만 테스트 코드 커버
       throw new Error("Value can not be greater than 100");
     }
     this.value = sum;
